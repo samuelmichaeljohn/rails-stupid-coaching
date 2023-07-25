722b34eae72c90ee76568d805e0be3ed855ead9d
samuel smith <samuel-smith@hotmail.com>
2023-07-25 21:20:37 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+    @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+#   If the message is I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise the coach will answer I don't care, get dressed and go to work!
+end
